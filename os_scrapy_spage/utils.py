7ea2d706c75bca43c3fbf5b978a2d494b68b3560
Ozzy <cfhamlet@gmail.com>
2020-12-16 15:32:50 +0800

    add: rvs

--- os_scrapy_spage/utils.py
+++ os_scrapy_spage/utils.py
@@ -12,25 +12,28 @@ from scrapy.http.response import Response
 from twisted.python.failure import Failure
 
 
-def get_value(key, *objs, default=None):
-    for obj in objs:
+def get_value(key, *objs, rvs=False, default=None):
+    r = 1 if not rvs else -1
+    for obj in objs[::r]:
         if key in obj:
             return obj[key]
     return default
 
 
-def spage(response: Union[Type[Response], Failure, FetchRecord], **kwargs) -> bytes:
+def spage(
+    response: Union[Type[Response], Failure, FetchRecord], rvs=False, **kwargs
+) -> bytes:
     if not isinstance(response, FetchRecord):
         response = fetch_record(response)
-    return _spage(response, **kwargs)
+    return _spage(response, rvs=rvs, **kwargs)
 
 
-def _spage(item: FetchRecord, **kwargs) -> bytes:
+def _spage(item: FetchRecord, rvs=False, **kwargs) -> bytes:
     request = item["request"]
     response = item["response"]
     meta = item["meta"]
 
-    url = get_value("url", request, kwargs)
+    url = get_value("url", request, kwargs, rvs=rvs)
     inner_header = {}
 
     for ik, k, objs, dft in (
@@ -39,31 +42,35 @@ def _spage(item: FetchRecord, **kwargs) -> bytes:
         (InnerHeaderKeys.IP_ADDRESS, "ip_address", (response, kwargs), None),
         (InnerHeaderKeys.FETCH_IP, "spider_ip", (meta, kwargs), None),
     ):
-        v = get_value(k, *objs, default=dft)
+        v = get_value(k, *objs, rvs=rvs, default=dft)
         if v is not None:
             inner_header[ik] = v
 
     inner_header[InnerHeaderKeys.DIGEST] = get_value(
-        "spage_digest", meta, kwargs, default=md5(url.encode()).hexdigest().upper()
+        "spage_digest",
+        meta,
+        kwargs,
+        rvs=rvs,
+        default=md5(url.encode()).hexdigest().upper(),
     )
 
-    v = get_value("download_latency", meta, kwargs)
+    v = get_value("download_latency", meta, kwargs, rvs=rvs)
     if v is not None:
         inner_header["Download-Latency"] = round(v)
 
-    v = get_value("fetch_time", meta, kwargs)
+    v = get_value("fetch_time", meta, kwargs, rvs=rvs)
     if v is not None:
         inner_header[InnerHeaderKeys.FETCH_TIME] = time.strftime(
             TIME_FORMAT, time.localtime(v)
         )
 
-    v = get_value("redirect_urls", meta)
+    v = get_value("redirect_urls", meta, kwargs, rvs=rvs)
     if v is not None:
         inner_header["Redirect-Count"] = len(v)
         inner_header["Final-Redirect"] = v[-1]
 
-    status = response["status"]
-    body = get_value("body", response)
+    status = get_value("status", response, kwargs, rvs=rvs)
+    body = get_value("body", response, kwargs, rvs=rvs)
     if not (status.group == Group.HTTP and (status.code == 200 or status.code == 206)):
         inner_header[InnerHeaderKeys.TYPE] = RecordTypes.DELETED
         inner_header["DeadDelete"] = "0"
@@ -73,16 +80,17 @@ def _spage(item: FetchRecord, **kwargs) -> bytes:
         size = 0 if not body else len(body)
         inner_header[InnerHeaderKeys.ORIGINAL_SIZE] = size
 
-    v = get_value("headers", request)
+    v = get_value("headers", request, kwargs, rvs=rvs)
     if v is not None:
-        v = v.to_unicode_dict()
+        if hasattr(v, "to_unicode_dict"):
+            v = v.to_unicode_dict()
         if InnerHeaderKeys.USER_AGENT in v:
             inner_header[InnerHeaderKeys.USER_AGENT] = v[InnerHeaderKeys.USER_AGENT]
 
-    http_header = None
-    v = get_value("headers", response)
-    if v is not None:
-        http_header = v.to_unicode_dict()
+    http_header = get_value("headers", response, kwargs, rvs=rvs)
+    if http_header is not None:
+        if hasattr(http_header, "to_unicode_dict"):
+            http_header = http_header.to_unicode_dict()
 
     spage = BytesIO()
     write(
